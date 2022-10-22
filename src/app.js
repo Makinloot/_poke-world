a6431306a1b5d00bdf50acd412f4843db2ff7248
Tornike Epitashvili <itorn9777@gmail.com>
2022-10-22 17:14:10 +0400

    added pug template and basic frontend structure

--- src/app.js
+++ src/app.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
+app.set('views', path.join(__dirname, '..', 'views'));
+app.set('view engine', 'pug');
+
 export {
     app,
 }
