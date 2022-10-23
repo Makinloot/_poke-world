fc1e92b67e9e0ad1e3a061e60c3a2d8859dcc9cd
Tornike Epitashvili <itorn9777@gmail.com>
2022-10-24 03:02:32 +0400

    added 404 page, created temporary styles for other pages

--- src/routes/pokemon/pokemon.controller.js
+++ src/routes/pokemon/pokemon.controller.js
@@ -7,9 +7,12 @@ async function httpGetPokemon(req, res) {
         const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
         const api_res = await fetch(url);
         const api_data = await api_res.json();
+
         res.render('html/assets/pokemon', {
             name: api_data.name,
-            img_src: api_data.sprites.other['official-artwork'].front_default || api_data.sprites.front_default,
+            img_src: api_data.sprites.other['official-artwork'].front_default || api_data.sprites.front_default || 'https://i0.wp.com/learn.onemonth.com/wp-content/uploads/2017/08/1-10.png?fit=845%2C503&ssl=1',
+            stats: api_data.stats.map(item => item.base_stat),
+            stats_names: api_data.stats.map(item => item.stat.name)
         });
     } catch (error) {
         res.render('html/assets/404');
