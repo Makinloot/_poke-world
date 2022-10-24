4e621d41eb1dfb0ebeafbbe57f3cd32ec44b1c95
Tornike Epitashvili <itorn9777@gmail.com>
2022-10-25 01:14:00 +0400

    added temporary styles for pokemon page

--- src/routes/pokemon/pokemon.controller.js
+++ src/routes/pokemon/pokemon.controller.js
@@ -12,7 +12,12 @@ async function httpGetPokemon(req, res) {
             name: api_data.name,
             img_src: api_data.sprites.other['official-artwork'].front_default || api_data.sprites.front_default || 'https://i0.wp.com/learn.onemonth.com/wp-content/uploads/2017/08/1-10.png?fit=845%2C503&ssl=1',
             stats: api_data.stats.map(item => item.base_stat),
-            stats_names: api_data.stats.map(item => item.stat.name)
+            stats_names: api_data.stats.map(item => item.stat.name),
+            abilities: api_data.abilities.map(item => item.ability.name),
+            types: api_data.types.map(item => item.type.name),
+            base_experience: api_data.base_experience,
+            height: api_data.height,
+            weight: api_data.weight,
         });
     } catch (error) {
         res.render('html/assets/404');
