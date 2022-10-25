59c21070928a365c466bd756b85fca8c0f38dd97
Tornike Epitashvili <itorn9777@gmail.com>
2022-10-25 19:09:20 +0400

    fixed minor bugs, added styles to pokemon page

--- public/js/stats.js
+++ public/js/stats.js
@@ -1,17 +1,21 @@
+// fetch pokemon data according to 'pokemon-name' element innerText
 window.onload = async() => {
     const pokemonName = document.getElementById('pokemon-name').innerText.toLowerCase();
 
     const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
     const res = await fetch(url);
     const data = await res.json();
+    
+    // to see data uncomment below
+    // console.log(data);
 
-    console.log(data);
     showStats(data);
 }
 
+// display stats height according to stats amount
 function showStats(data) {
-    const statsWrappers = document.querySelectorAll('.stat-item');
 
+    const statsWrappers = document.querySelectorAll('.stat-item');
     data.stats.forEach((item, i) => {
         const statValue = item.base_stat;
         let html = `<div class="stat-item-inner flex-row">${statValue}</div>`;
@@ -19,11 +23,8 @@ function showStats(data) {
     });
 
     const statInner = document.querySelectorAll('.stat-item-inner');
-    const statHeight = document.querySelector('.stat-item-inner').offsetHeight;
-
     statInner.forEach((item, i) => {
         let blockAmount = item.innerText / 1.5;
-        console.log(blockAmount, i)
         statInner[i].style.height = `${blockAmount}%`;
     });
 }
