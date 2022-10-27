9fa52acf7eda0086eff3f92daf449ef49917a2ab
Tornike Epitashvili <itorn9777@gmail.com>
2022-10-28 01:51:43 +0400

    fixed pagination

--- public/js/pagination.js
+++ public/js/pagination.js
@@ -1,13 +1,25 @@
 let pagesLength = document.getElementById('last-page').href.split('=')[1];
 const paginationLinks = document.querySelectorAll('.pag-link');
 
-// TODO: fix pagination
+// hide first elements in pagination if user is on first page
 paginationLinks.forEach((item, i) => {
 
-    const itemHrefValue = item.href.split('=')[1];
-
-    if(itemHrefValue <= 0) {
+    const hrefValue = item.href.split('=')[1];
+    if(hrefValue <= 0) {
         item.classList.add('hidden');
-    }
-    
+    } 
+
 });
+
+const pageEndPoint = window.location.href.split('?')[1];
+const pagButtons = [
+    document.getElementById('next'),
+    document.getElementById('after-next'),
+    document.getElementById('next-icon'),
+    document.getElementById('last-page'),
+]
+// hide last elements in pagination if user is on last page
+if(pageEndPoint === `page=${pagesLength}`) {
+    console.log('red');
+    pagButtons.forEach(item => item.classList.add('hidden'));
+}
