fbfd05bf320d44c4978f24f64fda5a1d00e8d1a5
Tornike Epitashvili <itorn9777@gmail.com>
2022-10-30 00:29:30 +0400

    removed custom stats and added chart.js

--- public/js/stats.js
+++ public/js/stats.js
@@ -1,3 +1,4 @@
+import 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js';
 // fetch pokemon data according to 'pokemon-name' element innerText
 window.onload = async() => {
     const pokemonName = document.getElementById('pokemon-name').innerText.toLowerCase();
@@ -6,25 +7,61 @@ window.onload = async() => {
     const res = await fetch(url);
     const data = await res.json();
     
-    // to see data uncomment below
-    // console.log(data);
-
-    showStats(data);
+    const statsData = data.stats.map(item => item.base_stat);
+    const labelsData = data.stats.map(item => item.stat.name);
+    initChart(statsData, labelsData);
 }
 
-// display stats height according to stats amount
-function showStats(data) {
-
-    const statsWrappers = document.querySelectorAll('.stat-item');
-    data.stats.forEach((item, i) => {
-        const statValue = item.base_stat;
-        let html = `<div class="stat-item-inner flex-row">${statValue}</div>`;
-        statsWrappers[i].innerHTML += html;
+function initChart(stats, labels) {
+    const ctx = document.getElementById('stats-chart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Base stats',
+                data: stats,
+                backgroundColor: [
+                    'rgba(255, 99, 132, .7)',
+                    'rgba(54, 162, 235, .7)',
+                    'rgba(255, 206, 86, .7)',
+                    'rgba(75, 192, 192, .7)',
+                    'rgba(153, 102, 255, .7)',
+                    'rgba(255, 159, 64, .7)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false,
+                } 
+            }
+        }
     });
 
-    const statInner = document.querySelectorAll('.stat-item-inner');
-    statInner.forEach((item, i) => {
-        let blockAmount = item.innerText / 1.5;
-        statInner[i].style.height = `${blockAmount}%`;
-    });
 }
+
+
+// // display stats height according to stats amount
+// function showStats(data) {
+
+//     const statsWrappers = document.querySelectorAll('.stat-item');
+//     data.stats.forEach((item, i) => {
+//         const statValue = item.base_stat;
+//         let html = `<div class="stat-item-inner flex-row">${statValue}</div>`;
+//         statsWrappers[i].innerHTML += html;
+//     });
+
+//     const statInner = document.querySelectorAll('.stat-item-inner');
+//     statInner.forEach((item, i) => {
+//         let blockAmount = item.innerText / 1.5;
+//         statInner[i].style.height = `${blockAmount}%`;
+//     });
+// }
